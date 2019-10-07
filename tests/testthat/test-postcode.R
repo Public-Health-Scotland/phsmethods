f43e920ec5a802fe0472ebf8e61de6dd85d3d830
Jack Hannah <jack.hannah1@nhs.net>
2019-10-07 18:07:19 +0100

    first tests

--- tests/testthat/test-postcode.R
+++ tests/testthat/test-postcode.R
@@ -1,5 +1,15 @@
 context("test-postcode")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Creates strings of correct length", {
+  expect_equal(stringr::str_length(postcode("G26QE")), 7)
+  expect_equal(stringr::str_length(postcode("KA89NB")), 7)
+  expect_equal(stringr::str_length(postcode("PA152TY")), 7)
+})
+
+test_that("Parses multiple input formats", {
+  input <- c("G429BA", "g429ba", "G42 9BA", "G 4 2 9 B A", "G429b    a")
+  formatted <- suppressWarnings(postcode(input))
+
+  expect_true(length(unique(formatted)) == 1)
+  expect_equal(unique(formatted), "G42 9BA")
 })
