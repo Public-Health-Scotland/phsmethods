ed7a337b6243f8ef4a518503dcb9b27f2e717cfe
Jack Hannah <jack.hannah1@nhs.net>
2019-10-07 16:26:35 +0100

    extra warning message

--- R/postcode.R
+++ R/postcode.R
@@ -2,31 +2,38 @@
 
 postcode <- function(string) {
 
-  if(!any(is.na(string)) &&
+  if(!any(is.na(string)) &
      !all(stringr::str_detect(string, "^[A-Za-z0-9 ]*$"))) {
     stop("Non-NA values in the input string(s) may contain letters, numbers ",
          "and spaces only")
   }
 
-  if(!any(is.na(string)) &&
+  if(!any(is.na(string)) &
      !all(stringr::str_detect(string, "(?=.*[0-9])(?=.*[A-Za-z])"))) {
     stop("Non-NA values in the input string(s) must contain both letters and ",
          "numbers")
   }
 
-  if(!any(is.na(string)) && any(grepl("[a-z]", string))) {
+  if(!any(is.na(string)) & any(grepl("[a-z]", string))) {
     warning("Any lower case letters in the input string(s) will be converted ",
             "to upper case")
   }
 
   string <- stringr::str_to_upper(string)
 
-  string <- gsub("\\s", "", string)
+  pc <- gsub("\\s", "", string)
+
+  if(!any(is.na(pc)) & !any(stringr::str_length(pc) %in% 5:7)) {
+    warning("Non-NA values in the input string(s) containing fewer than 5 or ",
+            "more than 7 alphanumeric characters will not have their spacing ",
+            "altered")
+  }
 
   dplyr::case_when(
-    is.na(string) ~ NA_character_,
-    stringr::str_length(string) == 5 ~ sub("(.{2})", "\\1  ", string),
-    stringr::str_length(string) == 6 ~ sub("(.{3})", "\\1 ", string),
+    is.na(pc) ~ NA_character_,
+    stringr::str_length(pc) == 5 ~ sub("(.{2})", "\\1  ", pc),
+    stringr::str_length(pc) == 6 ~ sub("(.{3})", "\\1 ", pc),
+    stringr::str_length(pc) == 7 ~ pc,
     TRUE ~ string
   )
 }
