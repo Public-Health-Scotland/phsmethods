c83ba844531ea50ff4ad42fda65f43ee0ec2d6f3
Jack Hannah <jack.hannah1@nhs.net>
2019-10-08 13:56:19 +0100

    add pc8 argument

--- R/postcode.R
+++ R/postcode.R
@@ -1,8 +1,14 @@
 #' @title Format a postcode
 #' @export
 
-postcode <- function(string) {
+postcode <- function(string, format = c("pc7", "pc8")) {
 
+  if(any(is.na(string))) {
+    warning("NA values in the input string(s) will be preserved")
+  }
+
+  # Convert NA to "NA" for error handling
+  # Will be converted back at the end
   string <- stringr::str_replace_na(string)
 
   if(!all(stringr::str_detect(string, "^[A-Za-z0-9 ]*$"))) {
@@ -20,25 +26,33 @@ postcode <- function(string) {
             "to upper case")
   }
 
-  if(any(is.na(string))) {
-    warning("NA values in the input string(s) will be preserved")
-  }
-
   string <- stringr::str_to_upper(string)
 
   pc <- gsub("\\s", "", string)
 
-  if(!all(stringr::str_length(pc) %in% 5:7)) {
+  if(!all(stringr::str_length(pc)[pc != "NA"] %in% 5:7)) {
     warning("Non-NA values in the input string(s) containing fewer than 5 or ",
             "more than 7 alphanumeric characters will not have their spacing ",
             "altered")
   }
 
-  dplyr::case_when(
-    pc == "NA" ~ NA_character_,
-    stringr::str_length(pc) == 5 ~ sub("(.{2})", "\\1  ", pc),
-    stringr::str_length(pc) == 6 ~ sub("(.{3})", "\\1 ", pc),
-    stringr::str_length(pc) == 7 ~ pc,
-    TRUE ~ string
-  )
+  if (format == "pc7") {
+    return(dplyr::case_when(
+      pc == "NA" ~ NA_character_,
+      stringr::str_length(pc) == 5 ~ sub("(.{2})", "\\1  ", pc),
+      stringr::str_length(pc) == 6 ~ sub("(.{3})", "\\1 ", pc),
+      stringr::str_length(pc) == 7 ~ pc,
+      TRUE ~ string
+    ))
+  }
+
+  if (format == "pc8") {
+    return(dplyr::case_when(
+      pc == "NA" ~ NA_character_,
+      stringr::str_length(pc) %in% 5:7 ~
+        stringi::stri_reverse(sub("(.{3})", "\\1 ", stringi::stri_reverse(pc))),
+      TRUE ~ string
+    ))
+  }
+
 }
