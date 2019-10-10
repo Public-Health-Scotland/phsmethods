4876d995407a13e1db27e76e501a32ada68b2e68
Jack Hannah <jack.hannah1@nhs.net>
2019-10-10 15:45:02 +0100

    restructure tests

--- tests/testthat/test-postcode.R
+++ tests/testthat/test-postcode.R
@@ -3,40 +3,35 @@ context("test-postcode")
 test_that("Creates strings of correct length", {
   expect_equal(stringr::str_length(postcode("G26QE", format = "pc7")), 7)
   expect_equal(stringr::str_length(postcode("G26QE", format = "pc8")), 6)
-  expect_equal(stringr::str_length(postcode("KA89NB", format = "pc7")), 7)
-  expect_equal(stringr::str_length(postcode("KA89NB", format = "pc8")), 7)
-  expect_equal(stringr::str_length(postcode("PA152TY", format = "pc7")), 7)
-  expect_equal(stringr::str_length(postcode("PA152TY", format = "pc8")), 8)
+  expect_equal(stringr::str_length(postcode(c("KA89NB", "PA152TY"),
+                                            format = "pc7")), c(7, 7))
+  expect_equal(stringr::str_length(postcode(c("KA89NB", "PA152TY"),
+                                            format = "pc8")), c(7, 8))
 })
 
 test_that("Parses multiple input formats", {
-  input_one <- c("G429BA", "g429ba", "G42 9BA", "G 4 2 9 B A", "G429b    a")
-  formatted_one <- suppressWarnings(postcode(input))
+  input_hampden <- c("G429BA", "g429ba", "G42 9BA", "G 4 2 9 B A", "G429b    a")
+  formatted_hampden <- suppressWarnings(postcode(input_hampden))
 
-  expect_true(length(unique(formatted_one)) == 1)
-  expect_equal(unique(formatted_one), "G42 9BA")
+  expect_true(length(unique(formatted_hampden)) == 1)
+  expect_equal(unique(formatted_hampden), "G42 9BA")
 })
 
-test_that("Errors if supplied with any incorrect input", {
-  expect_error(postcode("G2?QE"))
-  expect_error(postcode(c("EH7 5QG", "EH11 2NL", "EH5 2HF*")))
-  expect_error(postcode(2))
-  expect_error(postcode(NA))
+test_that("Correctly handles values which don't adhere to standard format", {
+  expect_true(is.na(suppressWarnings(postcode("G2?QE"))))
+  expect_warning(postcode(c("G207AL", "G2O7AL")))
+  expect_equal(suppressWarnings(postcode(c("EH7 5QG", NA,
+                                           "EH11 2NL", "EH5 2HF*"))),
+               c("EH7 5QG", NA, "EH112NL", NA))
 })
 
 test_that("Produces correct number of warning messages", {
-  input_two <- c("ab245qh", NA, "ab245q", "A  B245QH")
-  warnings_two <- capture_warnings(postcode(input_two))
+  input_dens <- c("Dd37Jy", "DD37JY", "D  d 337JY")
+  warnings_dens <- capture_warnings(postcode(input_dens))
+  expect_length(warnings_dens, 1)
 
-  expect_length(warnings_two, 2)
-
+  input_pittodrie <- c("ab245qh", NA, "ab245q", "A  B245QH")
+  warnings_pittodrie <- capture_warnings(postcode(input_pittodrie))
+  expect_length(warnings_pittodrie, 2)
 })
 
-test_that("Formats valid postcodes from vector containing invalid entries", {
-  mixed <- c("G207AL", "g2o7al", 6, "g 6 89ne", NA, "G43  2XR")
-  formatted_mixed <- suppressWarnings(postcode(mixed))
-
-  expect_equal(formatted_mixed,
-               c("G20 7AL", "g2o7al", "6", "G68 9NE", NA, "G43 2XR"))
-
-})
