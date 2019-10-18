d13f5360cf7340b873f7c4db0b1651a1a252b28e
CiaraGribben <ciaragribben@nhs.net>
2019-10-18 12:06:57 +0100

    formatting title, description etc.

--- R/financialyear.R
+++ R/financialyear.R
@@ -1,15 +1,20 @@
-#` @title Assign a date to the correct financial year
-#`
-#` @description \code{fin_year} takes a date and assigns it to the correct
-#` financial year in the PHI specified format.
-#`
-#` @details The PHI accepted format for financial year is yyyy/yy e.g. 2017/18.
-#`
-#` @param date A date which must be supplied in date format, The function
-#` as.Date() can be used to change a variable to date format.
-
-
-### Build function
+#' @title Format a postcode
+#'
+#' @description \code{fin_year} takes a date and assigns it to the correct
+#' financial year in the PHI specified format.
+#'
+#' @details The PHI accepted format for financial year is yyyy/yy e.g. 2017/18.
+#'
+#' @param date A date which must be supplied in standard date format. The
+#' function as.Date() or dmy (from lubridate) can be used to change a variable
+#' to date format.
+#'
+#' @examples
+#' x <- lubridate::dmy(c("21012017", "04042017", "17112017"))
+#'
+#' fin_year(x)
+#'
+#' @export
 fin_year <- function(date){
   if(class(date) != "Date"){
     stop("The date must be supplied in date format.")
@@ -21,9 +26,3 @@ fin_year <- function(date){
                        lubridate::year(date) + 1,
                        lubridate::year(date)), 3, 4))
 }
-
-
-
-
-
-
