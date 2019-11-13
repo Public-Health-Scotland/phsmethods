fa44a54ca1871fcb248f9ff938217b278bc19eae
CiaraGribben <ciaragribben@nhs.net>
2019-11-13 16:05:44 +0000

    Updating @param and error handling to use 'class' instead of 'format'

--- R/financialyear.R
+++ R/financialyear.R
@@ -5,9 +5,9 @@
 #'
 #' @details The PHI accepted format for financial year is yyyy/yy e.g. 2017/18.
 #'
-#' @param date A date which must be supplied in standard date format. The
-#' function as.Date() or dmy (from lubridate) can be used to change a variable
-#' to date format.
+#' @param date A date which must be supplied with \code{Date} class. The
+#' functions as.Date() or lubridate::dmy() are examples of functions that can
+#' be used to change a variable to date class.
 #'
 #' @examples
 #' x <- lubridate::dmy(c("21012017", "04042017", "17112017"))
@@ -17,7 +17,7 @@
 #' @export
 fin_year <- function(date){
   if(class(date) != "Date"){
-    stop("The date must be supplied in date format.")
+    stop("The input must have Date class.")
   }
   paste0(ifelse(lubridate::month(date) >= 4,
                 lubridate::year(date),
