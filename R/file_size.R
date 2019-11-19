df9c5a2898e4bb026caf2ff16b9df062933a2d02
Jack Hannah <jack.hannah1@nhs.net>
2019-11-19 12:47:29 +0000

    start documentation

--- R/file_size.R
+++ R/file_size.R
@@ -1,4 +1,26 @@
-#' @title Calculate file sizes
+#' @title Calculate file size
+#'
+#' @description \code{file_size} takes a filepath and an optional regex pattern.
+#' It returns the size of any files within that directory which match the given
+#' pattern.
+#'
+#' @param filepath A filepath. Defaults to the working directory,
+#' \code{getwd()}.
+#' @inheritParams base::list.files
+#'
+#' @return A \code{\link[tibble]{tibble}} listing the names of the files within
+#' \code{filepath} which match \code{pattern} and their respective file sizes.
+#' If no \code{pattern} is specified, \code{file_size} returns the names and
+#' file sizes of all files within \code{filepath}.
+#'
+#' If \code{filepath} is an empty folder, or \code{pattern} matches no files
+#' within \code{filepath}, \code{file_size} returns \code{NULL}.
+#'
+#' @seealso For more information on using regular expressions, see this
+#' \href{https://www.jumpingrivers.com/blog/regular-expressions-every-r-programmer-should-know/}{Jumping Rivers blog post}
+#' and this
+#' \href{https://stringr.tidyverse.org/articles/regular-expressions.html}{vignette}
+#' from the \href{https://stringr.tidyverse.org/}{stringr} package.
 #'
 #' @importFrom magrittr %>%
 #'
