c2f877bf4aa111d9af5f8224bfad13c9c7cf5c3d
Jack Hannah <jack.hannah1@nhs.net>
2020-01-09 15:40:21 +0000

    add extra test for qtr_next

--- tests/testthat/test-qtr.R
+++ tests/testthat/test-qtr.R
@@ -9,6 +9,14 @@ test_that("Returns correct quarter in correct format", {
                qtr(date = lubridate::dmy(15102018), format = "short"))
   expect_equal("October to December 2018",
                qtr(date = lubridate::dmy(15102018), format = "long"))
+  expect_equal("Sep 2018",
+               qtr_end(date = lubridate::dmy(03072018), format = "short"))
+  expect_equal("September 2018",
+               qtr_end(date = lubridate::dmy(03072018), format = "long"))
+  expect_equal("Dec 2018",
+               qtr_end(date = lubridate::dmy(15112018), format = "short"))
+  expect_equal("December 2018",
+               qtr_end(date = lubridate::dmy(15112018), format = "long"))
   expect_equal("Oct-Dec 1998",
                qtr_next(date = lubridate::dmy(02091998), format = "short"))
   expect_equal("October to December 1998",
@@ -25,19 +33,12 @@ test_that("Returns correct quarter in correct format", {
                qtr_prev(date = lubridate::dmy(15102018), format = "short"))
   expect_equal("July to September 2018",
                qtr_prev(date = lubridate::dmy(15102018), format = "long"))
-  expect_equal("Sep 2018",
-               qtr_end(date = lubridate::dmy(03072018), format = "short"))
-  expect_equal("September 2018",
-               qtr_end(date = lubridate::dmy(03072018), format = "long"))
-  expect_equal("Dec 2018",
-               qtr_end(date = lubridate::dmy(15112018), format = "short"))
-  expect_equal("December 2018",
-               qtr_end(date = lubridate::dmy(15112018), format = "long"))
 })
 
 test_that("Errors if the input date is not in date format", {
   expect_error(qtr(date = "2018-03-25", format = "short"))
   expect_error(qtr_end(date = as.factor("2018-03-25"), format = "long"))
+  expect_error(qtr_next(date = NULL, format = "short"))
   expect_error(qtr_prev(date = as.numeric(lubridate::dmy(25032018)),
-                        format = "short"))
+                        format = "long"))
 })
