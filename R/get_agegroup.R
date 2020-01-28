bcc62e3d96558a0a51625c876649e99012175b71
chrisdeans <chrisdeans@nhs.net>
2020-01-28 09:28:21 +0000

    Added get_agegroup function plus tests

---
+++ R/get_agegroup.R
@@ -0,0 +1,113 @@
+#' Create age groups
+#'
+#' @description
+#' A wrapper for \code{\link[base]{cut}} tailored for creating age groups. For
+#' each value of integer \code{x}, what age group does it fall into, based on a
+#' given set of age group breakpoints.
+#'
+#' @param x a vector of numeric values
+#' @param breaks a vector of numeric values to be used as breakpoints. These are
+#'   sorted before use by the function. The defualt value is the agegroups of the
+#'   \href{https://www.opendata.nhs.scot/dataset/standard-populations/resource/edee9731-daf7-4e0d-b525-e4c1469b8f69}{European Standard Population}.
+#' @param include_zero If \code{TRUE}, zero will be appended to the start of the
+#'   breakpoints (unless it is already included). If \code{FALSE}, the function
+#'   will return \code{NA} for any value less than the smallest breakpoint.
+#' @param include_inf If \code{TRUE}, \code{Inf} will be appended to the end of
+#'   the breakpoints (unless it is already included). If \code{FALSE}, the
+#'   function return \code{NA} for any value greater than or equal to the
+#'   largest breakpoint.
+#' @param as_factor The default behaviour is to return a factor vector. Use
+#'   \code{FALSE} to return a character vector instead.
+#'
+#' @return A factor vector, where each element corresponds to the same element
+#'   in \code{x}. If \code{as_factor = FALSE} a character vector is returned
+#'   instead.
+#'
+#' @note
+#' The breakpoints are used to create distinct contiguous age groups which cover
+#' the whole range between the smallest and largest points (or zero and
+#' \code{Inf} depending on which options are used). A single call of the
+#' function cannot create overlapping age groups (e.g. 65+ and 75+).
+#'
+#' @export
+#' @examples
+#' age <- c(54, 7, 77, 1, 26, 58, 46, 79, 31, 85, 13, 101)
+#' get_agegroup(age)
+#'
+#' get_agegroup(age, c(5, 15, 25, 45, 65, 75, 85))
+#'
+#' #If \code{include_zero} or \code{include_inf} are set to \code{FALSE}, values
+#' #outside of the range will return \code{NA}:
+#' get_agegroup(age, c(5, 15, 25, 45, 65, 75, 85), include_zero = FALSE)
+#'
+#' #Other functions can be used to specify the breaks, e.g. \code\link[base]{seq}:
+#' get_agegroup(age, seq(0, 100, by = 10))
+#'
+#' #To get the output as a vector of character:
+#' get_agegroup(age, as_factor = FALSE)
+
+get_agegroup <- function(x,
+                         breaks = c(seq(0, 90, by = 5), Inf),
+                         include_zero = TRUE,
+                         include_inf = TRUE,
+                         as_factor = TRUE) {
+
+  if(is.null(x))
+    stop("argument 'x' is missing or NULL, with no default")
+
+  if(!is.numeric(x)){
+    warning("Non-numeric values found in 'x'. Attempting to convert to numeric")
+    x <- as.numeric(x)
+  }
+
+  ### Handling for breaks vector ----
+  if(!is.numeric(breaks))
+    stop("non-numeric value found in 'breaks'")
+
+  if(any(breaks < 0))
+    warning("Negative values in breaks. Check output carefully")
+
+  if(any(trunc(breaks) != breaks))
+    warning("Non-integer values found in breaks. Group labels may not be correct")
+
+  # If include_zero is true (default) and last break value is not already 0
+  # add 0 to the break vector
+  if(include_zero & !(0 %in% breaks))
+    breaks <- c(0, breaks)
+
+  # If include_inf is true (default) and last break value is not already Inf
+  # add Inf to the break vector
+  if(include_inf & !(Inf %in% breaks))
+    breaks <- c(breaks, Inf)
+
+  # Sort breaks so the limits are in the correct order
+  breaks <- sort(unique(breaks))
+
+  # If there's only one break value, add a second value so cut function will run
+  if(length(breaks) == 1)
+    breaks <- c(breaks, breaks+1)
+
+  ### Creation of labels list ----
+  # Create labels based on consecutive values in breaks
+  labels <- paste(utils::head(breaks, -1), utils::tail(breaks,-1)-1, sep="-")
+
+  # Reformat label for last value (if appropriate)
+  labels <- gsub("-Inf", "+", labels)
+
+  # Reformat labels for single values (if appropriate)
+  labels[utils::head(breaks, -1) == utils::tail(breaks, -1)-1] <-
+    utils::head(breaks, -1)[utils::head(breaks, -1) == utils::tail(breaks, -1)-1]
+
+  ### Age grouping ----
+  # Create age groups
+  agegroup <- cut(x,
+                  breaks = breaks,
+                  labels = labels,
+                  right = FALSE,
+                  ordered_result = TRUE)
+
+  if(as_factor == F)
+    agegroup <- as.character(agegroup)
+
+  return(agegroup)
+}
---
+++ tests/testthat/test-get_agegroup.R
@@ -0,0 +1,127 @@
+context("test-get_agegroup")
+
+test_that("Return correct age groups", {
+  expect_identical(
+    get_agegroup(c(4, 51, 21, 89),
+                 breaks = seq(0, 90, 10),
+                 as_factor = F),
+    c("0-9", "50-59", "20-29", "80-89"))
+
+  expect_identical(
+    get_agegroup(c(8, 94, 44, 55, 14),
+                 breaks = seq(0, 90, 5)),
+    factor(c("5-9", "90+", "40-44", "55-59", "10-14"),
+           levels = c("0-4", "5-9", "10-14", "15-19","20-24", "25-29", "30-34",
+                      "35-39", "40-44", "45-49", "50-54", "55-59", "60-64",
+                      "65-69", "70-74", "75-79", "80-84", "85-89", "90+"),
+           ordered = T))
+
+  expect_identical(
+    get_agegroup(c(81, 86, 33, 11),
+                 breaks=c(86, 34, 32, 87)),
+    factor(c("34-85", "86", "32-33", "0-31"),
+           levels = c("0-31", "32-33", "34-85", "86", "87+"),
+           ordered = T))
+
+  expect_identical(
+    get_agegroup(c(0, 99, 1000, 5, 5),
+                 breaks = seq(0, 90, 5),
+                 as_factor = F),
+    c("0-4", "90+", "90+", "5-9", "5-9"))
+})
+
+test_that("Default value for breaks", {
+  expect_identical(
+    get_agegroup(10),
+    factor(c("10-14"),
+           levels = c("0-4", "5-9", "10-14", "15-19", "20-24", "25-29", "30-34",
+                      "35-39", "40-44", "45-49", "50-54", "55-59", "60-64",
+                      "65-69", "70-74", "75-79", "80-84", "85-89", "90+"),
+           ordered = T))
+})
+
+
+test_that("Non-numeric values for x converted with warning", {
+  expect_warning(get_agegroup("test"))
+
+  expect_warning(a <- get_agegroup(c("26", "9", "78", "81"), as_factor = F))
+  expect_equal(a, c("25-29", "5-9", "75-79", "80-84"))
+
+  expect_warning(a <- get_agegroup(c("1", "57", "apple", "12"), as_factor = F))
+  expect_equal(a, c("0-4", "55-59", NA, "10-14"))
+})
+
+test_that("Out of order breaks", {
+  expect_identical(
+    get_agegroup(c(7, 21, 14),
+                 breaks = c(15, 5, 20, 10)),
+    factor(c("5-9", "20+", "10-14"),
+           levels = c("0-4", "5-9", "10-14", "15-19", "20+"),
+           ordered = T))
+})
+
+test_that("Duplicate breaks", {
+  expect_identical(
+    get_agegroup(c(7, 21, 14),
+                 breaks = c(5, 10, 10, 15, 5, 20)),
+    factor(c("5-9", "20+", "10-14"),
+           levels = c("0-4", "5-9", "10-14", "15-19", "20+"),
+           ordered = T))
+})
+
+test_that("Excluding zero", {
+  expect_identical(
+    get_agegroup(c(7, 21, 14),
+                 breaks = c(10, 20),
+                 include_zero = F),
+    factor(c(NA, "20+", "10-19"),
+           levels = c("10-19", "20+"),
+           ordered = T))
+})
+
+test_that("Excluding infinity", {
+  expect_identical(
+    get_agegroup(c(7, 21, 14),
+                 breaks = c(10, 20),
+                 include_inf = F),
+    factor(c("0-9", NA, "10-19"),
+           levels = c("0-9", "10-19"),
+           ordered = T))
+})
+
+test_that("Exclude zero and infinity", {
+  expect_identical(
+    get_agegroup(c(7, 21, 14),
+                 breaks = c(10, 20),
+                 include_inf = F,
+                 include_zero = F),
+    factor(c(NA, NA, "10-19"),
+           levels = c("10-19"),
+           ordered = T))
+})
+
+test_that("Single break value", {
+  expect_identical(
+    get_agegroup(c(7, 21, 14),
+                 breaks = 21,
+                 include_inf = F,
+                 include_zero = F),
+    factor(c(NA, "21",  NA),
+           levels = "21",
+           ordered = T))
+})
+
+test_that("Negative values in breaks gives warning", {
+  expect_warning(a <- get_agegroup(c(-7, -1, 0, 4), breaks = c(-10, 10)))
+  expect_identical(a, factor(c("-10--1", "-10--1",  "0-9", "0-9"),
+                             levels = c("-10--1", "0-9", "10+"),
+                             ordered = T))
+})
+
+test_that("Fractional values in breaks gives warning", {
+  expect_warning(
+    a <- get_agegroup(c(7, 21, 14, 10.4, 10.6), breaks = c(5, 10.5, 20)))
+  expect_identical(a, factor(c("5-9.5", "20+", "10.5-19", "5-9.5", "10.5-19"),
+                             levels = c("0-4", "5-9.5", "10.5-19", "20+"),
+                             ordered = T))
+})
