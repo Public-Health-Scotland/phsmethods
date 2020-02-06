34f9ff50aa4184f258437f3f42c8af14b6e978ab
David Caldwell <david.caldwell1@nhs.net>
2020-02-06 09:34:41 +0000

    Add a note explaining methodology

--- R/fin_year.R
+++ R/fin_year.R
@@ -21,6 +21,12 @@ fin_year <- function(date) {
     stop("The input must have Date or POSIXct class.")
   }
 
+  # Simply converting all elements of the input vector resulted in poor
+  # performance for large vectors. The function was rewritten to extract
+  # a vector of unique elements from the input, convert those to financial year
+  # and then match them back on to the original input. This vastly improves
+  # performance for large inputs
+
   unique_dates <- tibble::tibble(dates = unique(date)) %>%
 
     dplyr::mutate(fin_year = paste0(ifelse(lubridate::month(.data$dates) >= 4,
