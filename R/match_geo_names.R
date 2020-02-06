a97c80ff829579f0c97dfff29d6e58668c03ff51
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-02-06 11:18:55 +0000

    extracting from SG open data

--- R/match_geo_names.R
+++ R/match_geo_names.R
@@ -1,6 +1,6 @@
 #' @title Match geography codes with their names
 #'
-#' @description \code{match_geo_names} takes a geography code and brings back the
+#' @description \code{match_area_names} takes a geography code and brings back the
 #' name of the area.
 #'
 #' @details It uses the standard 9 digit codes for matching with the names.
@@ -8,8 +8,10 @@
 #' 2014 and 2019 Health Boards).
 #'
 #' It works for Health Boards, Council Areas, Health and Social Care Partnerships,
-#' Intermediate Zones, Data Zones, Electoral Wards, Scottish Parliamentary
-#' Constituencies and Regions, and UK Parliamentary Constituencies.
+#' Intermediate Zones, Data Zones (2001 and 2011), Electoral Wards,
+#' Scottish Parliamentary Constituencies, UK Parliamentary Constituencies,
+#' Travel to work areas, National Parks, Commmunity Health Partnerships,
+#' Localities (S19), Settlements (S20) and Scotland
 #'
 #' @param dataset Data frame/tibble that contains the geography code variable.
 #' @param code_var The variable that contains the geography codes. Needs to be
@@ -26,50 +28,52 @@
 #' @export
 match_area_names <- function(dataset, code_var) {
 
-  # Folder where all the lookups between codes and names sit
-  cl_out <- paste0("/conf/linkage/output/lookups/Unicode/Geography/",
-                   "Scottish Postcode Directory/Codes and Names/")
+###############################################.
+## Creating lookup from SG open data platform ----
+###############################################.
 
-  # List of name-code files in cl-out
-  files <-  list.files(path = cl_out, pattern = "*.csv", full.names = TRUE)
+  # If the reference file exists it moves to the merging with the dataset
+  if (file.exists("reference_files/area_name_lookup.rds") == FALSE) {
 
-  # Selecting name files for main geographies that would be included in lookup
-  files_wanted <- paste0("Health Board Area|Council Area|Intermediate Zone|",
-                         "Data Zone|Integration Authority|",
-                         "Parliamentary|Electoral")
-  # selecting only those of interest
-  files <- files[grepl(files_wanted, files) == T]
+    # Extracting the lookup between codes and names from SG open data platform
+    endpoint <- "http://statistics.gov.scot/sparql" # api adress
 
-  # Creating names lookup for all geographies.
-  # Read and combines all files in folder
-  names_lookup <- do.call(dplyr::bind_rows, lapply(files, function(x){
-    data.table::fread(x) %>% janitor::clean_names()# variables to lower case
-  }))
+    # Query for the platform API, written in SPARQL
+    # the optional means it extracts all codes even when they don't have a name.
+    query_names <- "SELECT ?geo_code ?area_name
+    WHERE {
+    ?s <http://statistics.data.gov.uk/def/statistical-entity#code> ?entity;
+    <http://www.w3.org/2004/02/skos/core#notation> ?geo_code.
+    OPTIONAL {?s <http://statistics.data.gov.uk/def/statistical-geography#officialname> ?area_name.}
+    }
+    ORDER BY ?geo_code "
 
-  # Now formatting it in a long format with only two columns: code and area_name.
-  names_lookup %<>%
-    dplyr::select(-nrs_health_board_area_name) %>% # duplicated name column
-    dplyr::mutate_all(as.character) %>% # as there are a couple of integer ones
-    # Moving all codes and all names on their own columns
-    tidyr::pivot_longer(cols = contains("name"), names_to = "index",
-                 values_to = "area_name") %>%
-    tidyr::pivot_longer(cols = contains("code"), names_to = "index2",
-                 values_to = "geo_code") %>%
-    # filtering out those rows of spurious combinations
-    dplyr::filter(!(is.na(geo_code)) & !(is.na(area_name))) %>%
-    # Selecting only one row of names per code
-    # (e.g. a CA with the same code in different versions)
-    dplyr::select(-index, -index2) %>%
-    unique()
+    qd <- SPARQL::SPARQL(endpoint, query_names)
 
-  # Adding a few extra codes and names not present in lookup files
-  other_names <- data.frame(area_name = c(rep("Scotland", 2),
-                                          "Non-NHS Provider/Location",
-                                          "Not applicable",
-                                          "Golden Jubilee Hospital"),
-                            geo_code = c("S00000001", "S92000003", "S27000001",
-                                         "S27000002", "S08100001"))
-  names_lookup <- rbind(names_lookup, other_names)
+    names_lookup <- qd[["results"]] %>%
+      dplyr::mutate(geo_code = substr(geo_code, 2, 10)) # extracting only code
+
+    # Adding a few extra codes and names not present in lookup files
+    other_names <- tibble::tibble(
+      area_name = c("Scotland", "Non-NHS Provider/Location", "Not applicable",
+                    "Golden Jubilee Hospital", "The State Hospital",
+                    "No Fixed Abode", "Rest of UK (Outside Scotland)",
+                    "Outside the UK", "Unknown residency",
+                    "Rest of UK (Outside Scotland)", "No Fixed Abode",
+                    "Unknown residency", "Outside the UK"),
+      geo_code = c("S00000001", "S27000001", "S27000002", "S08100001", "S08100008",
+                   sprintf("RA270%d", seq(1:4)), sprintf("S0820000%d", seq(1:4))))
+
+    names_lookup <- rbind(names_lookup, other_names)
+
+    saveRDS(names_lookup, "reference_files/area_name_lookup.rds")
+  }
+
+  ###############################################.
+  ## Merging dataset with names lookup ----
+  ###############################################.
+  # Read in area name to geographic code lookup
+  names_lookup <- readRDS("reference_files/area_name_lookup.rds")
 
   # Ensuring geo_code variable is of the right type
   dataset %<>% dplyr::mutate_at(code_var, as.character)
