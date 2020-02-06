1146a2aaeeeedd1aa2fbbc2ca3959878f22cc649
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-02-06 16:44:05 +0000

    adding some more tests

--- tests/testthat/test-match_area_names.R
+++ tests/testthat/test-match_area_names.R
@@ -1,27 +1,40 @@
 context("test-match_area_names")
 
-test_that("Expect the same names as in sample dataframe", {
+testthat::test_that("Expect the same names as in sample dataframe", {
   test_df <- readRDS("tests/testthat/files/code_names_sample.rds") %>%
     match_area_names("code") %>%
-    mutate(check = ifelse(name_orig == area_name, TRUE, FALSE))
+    dplyr::mutate(check = ifelse(name_orig == area_name, TRUE, FALSE))
 
-  expect_true(all(test_df$check == T))
+  testthat::expect_true(all(test_df$check == T))
 
 })
 
+testthat::test_that("Not necessarily an error, but if less than 18 some geographic
+          entities might have dissapeared, if more some new ones could have been
+          added and both cases might merit a check", {
 
-test_that("Not necessarily an error, but if less than 19 some files might have dissapeared,
-          if more some new ones could have been added and this might merit a check", {
-  cl_out <- "/conf/linkage/output/lookups/Unicode/Geography/Scottish Postcode Directory/Codes and Names/"
+  # Read in area name to geographic code lookup
+  names_lookup <- readRDS("reference_files/area_name_lookup.rds")
 
-  # Selecting name files for main geographies
-  files_wanted <- paste0("Health Board Area|Council Area|Intermediate Zone|Data Zone|",
-                         "Integration Authority|Parliamentary|Electoral")
-  # List of name-code files in cl-out
-  files <-  list.files(path = cl_out, pattern = "*.csv", full.names = TRUE)
-  files <- files[grepl(files_wanted, files) == TRUE] #selecting only those of interest
+  number_areas <- length(unique(substr(names_lookup$geo_code, 1, 3)))
 
-  expect_true(length(files) == 19)
+  testthat::expect_true(number_areas == 18)
+
+          })
+
+testthat::test_that("Expect an error if object not a dataframe", {
+
+  testthat::expect_error(test_object <- list() %>%
+                           match_area_names("code") )
+
+})
+
+testthat::test_that("Expect an error if code variable is numeric", {
+
+  testthat::expect_error(
+    test_df <- data.frame(code = 1:10) %>%
+      match_area_names("code")
+  )
 
 })
 
