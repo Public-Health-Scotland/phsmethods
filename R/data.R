5df9687ad2c6d57875a4ced9979f871911d625c2
Jaime Villacampa <jaime.villacampa@nhs.net>
2020-03-04 14:10:09 +0000

    documentation of dataset

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Codes and names of Scottish geographical and administrative areas.
+#'
+#' A dataset containing the codes and names of areas used in Scotland.
+#'
+#' @format A data frame with 2 variables and over 23000 rows:
+#' \describe{
+#'   \item{geo_code}{Standard geography code - 9 characters}
+#'   \item{area_name}{Name of the area the code represents}
+#' }
+#' @source \url{https://statistics.gov.scot/}
+"area_name_lookup"
