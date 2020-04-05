8f7506cab51d700281ace33c5cd67a4cc2859e08
jackhannah95 <jack.hannah1@nhs.net>
2020-04-05 22:17:26 +0100

    I can't even begin to describe all this

--- R/match_area.R
+++ R/match_area.R
@@ -2,9 +2,9 @@
 #'
 #' @description \code{match_area} takes a vector of geography codes or area
 #' names. It matches the input to the corresponding value(s) in the
-#' \code{\link{area_lookup}} dataset. It returns the corresponding area name when
-#' supplied with a geography code, or the corresponding geography code(s) when
-#' supplied with an area name.
+#' \code{\link{area_lookup}} dataset. It returns the corresponding area name
+#' when supplied with a geography code, or the corresponding geography code(s)
+#' when supplied with an area name.
 #'
 #' @details \code{match_area} relies predominantly on the standard 9 digit
 #' geography codes. The only exceptions are:
@@ -36,8 +36,7 @@
 #' transcription between the input vector and the corresponding value in the
 #' lookup dataset.
 #'
-#' @param x An input vector of geography codes or area names. Can be of
-#' \code{character}, \code{list} or \code{factor} class.
+#' @param x An input vector of geography codes or area names.
 #' @param return A character string declaring the type of desired output.
 #' Valid options are `name` and `code`. When set to `name`, \code{match_area}
 #' returns area names. When set to `code`, \code{match_area} returns geography
@@ -58,6 +57,9 @@
 #' new variable in a \code{\link[base:data.frame]{data.frame()}} or
 #' \code{\link[tibble]{tibble}}.
 #'
+#' Any input value without a corresponding value in the
+#' \code{\link{area_lookup}} will return an NA output value.
+#'
 #' @examples
 #' match_area("S20000010")
 #' match_area(c("Ayr North", "Ayr East", "Ayr West"), return = "code")
@@ -72,21 +74,49 @@ match_area <- function(x, return = c("name", "code")) {
 
   return <- match.arg(return)
 
-  if (!inherits(x, c("character", "list", "factor"))) {
-    stop("The input vector must be of character, list or factor class")
-  }
-
   # Coerce input to character to prevent any warning messages appearing about
   # type conversion in dplyr::left_join
   x <- as.character(x)
 
+  # Calculate the number of non-NA input geography codes which are not 9
+  # characters in length or one of the exceptions
+  if (return == "name") {
+    n <- length(x[!is.na(x)][nchar(x[!is.na(x)]) != 9 &
+                               !x[!is.na(x)] %in% sprintf("RA270%d", seq(1:4))])
+
+  } else {
+
+    # This value is irrelevant if the function is provided with area names
+    # It's easier to set n equal to zero than to have nested if statements
+    n <- 0
+  }
+
+  # If n is one, the warning message describing the number of non-NA codes
+  # which are not length 9 or one of the exceptions should use singular verbs
+  # Otherwise, use plural ones
+  singular <- "code is"
+  multiple <- "codes are"
+
+  if (n > 0) {
+    warning(glue::glue("{n} non-NA input geography ",
+                       "{ifelse(n == 1, singular, multiple)} not 9 characters ",
+                       "in length and will return an NA. The only allowed ",
+                       "codes with a differing number of characters are:\n",
+                       "\U2022 RA2701: No Fixed Abode\n",
+                       "\U2022 RA2702: Rest of UK (Outside Scotland)\n",
+                       "\U2022 RA2703: Outside the UK\n",
+                       "\U2022 RA2704: Unknown Residency"))
+  }
+
   area_lookup <- phsmethods::area_lookup
 
-  if (return == "code") {
+  if (return == "name") {
 
-    return(dplyr::left_join(tibble::enframe(x,
-                                            name = NULL,
-                                            value = "geo_code"),
+    x <- tibble::enframe(x,
+                         name = NULL,
+                         value = "geo_code")
+
+    return(dplyr::left_join(x,
                             area_lookup,
                             by = "geo_code") %>%
 
@@ -95,9 +125,22 @@ match_area <- function(x, return = c("name", "code")) {
 
   } else {
 
-    return(dplyr::left_join(tibble::enframe(x,
-                                            name = NULL,
-                                            value = "area_name"),
+    x <- tibble::enframe(x,
+                         name = NULL,
+                         value = "area_name") %>%
+
+      # NA values will all be lumped together as a factor level, which will
+      # cause problems
+      # Replace each instance of NA with a character value of "NA.n", where .n
+      # represents the nth NA in the input vector
+      # These values won't be returned in the final output so it doesn't really
+      # matter how they're recorded, each one just needs to be unique
+      dplyr::mutate(area_name = replace(
+        area_name,
+        is.na(area_name),
+        sprintf("NA%d", seq(1, dplyr::n_distinct(is.na(area_name))))))
+
+    return(dplyr::left_join(x,
                             area_lookup,
                             by = "area_name") %>%
 
