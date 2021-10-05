597d3518eb4ddb78cf7b01fed6f43b5f76fa7bc4
Moohan <james.mcmahon@phs.scot>
2021-10-05 17:04:32 +0100

    Sample tests

---
+++ tests/testthat/test-dob_from_chi.R
@@ -0,0 +1,15 @@
+test_that("Returns correct DoB - no options", {
+
+})
+
+test_that("Returns correct DoB - options supplied", {
+
+})
+
+test_that("Returns NA when DoB is ambiguous", {
+
+})
+
+test_that("Supplied ages override supplied dates", {
+
+})
