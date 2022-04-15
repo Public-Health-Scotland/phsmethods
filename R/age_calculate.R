d3088c8737b9b77601a3f75fdaa116bb17fd0811
Tina Fu <yuyan.fu2@phs.scot>
2022-04-15 14:38:02 +0100

    update age_calculate function

--- R/age_calculate.R
+++ R/age_calculate.R
@@ -1,9 +1,6 @@
 #' Calculate age between two dates
 #'
-#' @description This function calculates the age between two dates using `lubridate` periods and durations.
-#' `lubridate` duration is measured in seconds whereas period is human interpretable
-#' unit and varies depending on the temporal context.
-#' Easily switch between the two with the `date_class` argument. Default is "period".
+#' @description This function calculates the age between two dates using `lubridate`.
 #'
 #' @param start A start date (e.g. date of birth) which must be supplied with \code{Date} or \code{POSIXct} or \code{POSIXlt}
 #' class. \code{\link[base:as.Date]{as.Date()}},
@@ -12,10 +9,8 @@
 #' can be used to store dates as an appropriate class.
 #' @param end An end date which must be supplied with \code{Date} or \code{POSIXct} or \code{POSIXlt} class.
 #' Default is `Sys.Date()` or `Sys.time()` depending on the class of `start`.
-#' @param units Type of units to be used. Seconds, minutes, hours, days, weeks, months and years are accepted.
+#' @param units Type of units to be used. years and months are accepted. Default is \code{years}.
 #' @param round_down Should returned ages be rounded down to the nearest whole number. Default is \code{TRUE}.
-#' @param date_class Type of `lubridate` date class. Accepted arguments are "period" and "duration".
-#' For details please see \code{\link[lubridate:as.period]{lubridate::as.period()}} and \code{\link[lubridate:as.duration]{lubridate::as.duration()}}.
 #' @examples
 #' \dontrun{
 #' library(phsmethods)
@@ -24,30 +19,20 @@
 #' end_date <- today()
 #' age_calculate(my_date, end_date)
 #' age_calculate(my_date, end_date, round_down = FALSE)
-#' age_calculate(my_date, end_date, round_down = FALSE, date_class = "period")
-#' age_calculate(my_date, end_date, units = "days")
-#'
-#' # Default date class is period
-#' age_calculate(my_date, end_date, round_down = FALSE) * 365.25
-#' age_calculate(my_date, end_date, round_down = FALSE, date_class = "duration") * 365.25
-#'
-#' # Can switch between date classes easily
-#' ( dst1 <- ymd_hms("2021-10-31 00:30:00", tz = "GB") )
-#' ( dst2 <- dst1 + days(1) )
-#' age_calculate(dst1, dst2, unit = "hours", date_class = "duration")
-#' age_calculate(dst1, dst2, unit = "hours", date_class = "period")
+#' age_calculate(my_date, end_date, units = "months")
 #'
 #' # It's worth noting that `lubridate` periods classify leap year birthdays
 #' # slightly differently to UK law where (in the UK) legally speaking
 #' # leaplings become a year older on the 1st March on non-leap years.
 #' leap1 <- dmy("29-02-2020")
 #' leap2 <- dmy("28-02-2022")
-#' age_calculate(leap1, leap2, date_class = "duration")
-#' age_calculate(leap1, leap2, date_class = "period")
+#' leap3 <- dmy("01-03-2022")
+#' age_calculate(leap1, leap2)
+#' age_calculate(leap1, leap3)
 #' }
 #' @export
 age_calculate <- function(start, end = if (lubridate::is.Date(start)) Sys.Date() else Sys.time(),
-                          units = "years", date_class = c("period", "duration"), round_down = TRUE){
+                          units = "years", round_down = TRUE){
 
   if (!inherits(start, c("Date", "POSIXt"))) {
     stop("The start date must have Date or POSIXct or POSIXlt class")
@@ -57,23 +42,14 @@ age_calculate <- function(start, end = if (lubridate::is.Date(start)) Sys.Date()
     stop("The end date must have Date or POSIXct or POSIXlt class")
   }
 
-  units <- match.arg(tolower(units), c("years", "months", "weeks", "days",
-                                       "hours", "minutes", "seconds"))
-
-  date_class <- match.arg(date_class)
+  units <- match.arg(tolower(units), c("years", "months"))
 
   age_interval <- lubridate::interval(start, end)
 
-  unit_time <- do.call(get(date_class, asNamespace("lubridate")),
+  unit_time <- do.call(get("period", asNamespace("lubridate")),
                        list(num = 1, units = units))
 
-  if (date_class == "duration") {
-    age_interval <- lubridate::as.duration(age_interval)
-  }
-
-  if (date_class == "period" & !units %in% c("week", "weeks")) {
-    age_interval <- lubridate::as.period(age_interval, unit = units)
-  }
+  age_interval <- lubridate::as.period(age_interval, unit = units)
 
   if (round_down) {
     age <- age_interval %/% unit_time
--- tests/testthat/test-age_calculate.R
+++ tests/testthat/test-age_calculate.R
@@ -1,81 +1,38 @@
 test_that("Age is calculated correctly for all units", {
   expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "years", date_class = "period",
-                             round_down = TRUE), 1)
+                             units = "years", round_down = TRUE), 1)
   expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "months", date_class = "period",
-                             round_down = TRUE), 12)
-  expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "weeks", date_class = "period",
-                             round_down = TRUE), 55)
-  expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "days", date_class = "period",
-                             round_down = TRUE), 385)
-  expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "hours", date_class = "period",
-                             round_down = TRUE), 9240)
-  expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "minutes", date_class = "period",
-                             round_down = TRUE), 554400)
-  expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "seconds", date_class = "period",
-                             round_down = TRUE), 33264000)
+                             units = "months", round_down = TRUE), 12)
 })
 
 test_that("Age is calculated correctly for leap years", {
   expect_equal(age_calculate(as.Date("2020-02-29"), as.Date("2022-02-28"),
-                             units = "years", date_class = "period",
-                             round_down = TRUE), 1)
+                             units = "years", round_down = TRUE), 1)
   expect_equal(age_calculate(as.Date("2020-02-29"), as.Date("2022-03-01"),
-                             units = "years", date_class = "period",
-                             round_down = TRUE), 2)
-  expect_equal(age_calculate(as.Date("2020-02-20"), as.Date("2020-03-01"),
-                             units = "days", date_class = "period",
-                             round_down = TRUE), 10)
+                             units = "years", round_down = TRUE), 2)
 })
 
 test_that("Non-date formats for start or end date produce an error", {
   expect_error(age_calculate("01022021", as.Date("2022-02-21"),
-                             units = "years", date_class = "period",
-                             round_down = TRUE))
+                             units = "years", round_down = TRUE))
   expect_error(age_calculate(as.Date("2021-02-01"), "21022022",
-                             units = "years", date_class = "period",
-                             round_down = TRUE))
+                             units = "years", round_down = TRUE))
   expect_error(age_calculate("2021-02-01", "2022-02-21",
-                             units = "years", date_class = "period",
-                             round_down = TRUE))
+                             units = "years", round_down = TRUE))
   expect_error(age_calculate("2021-02-01", "21-Feb-2022",
-                             units = "years", date_class = "period",
-                             round_down = TRUE))
+                             units = "years", round_down = TRUE))
 })
 
 test_that("Upper case units can be dealt with", {
   expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "YEARS", date_class = "period",
-                             round_down = TRUE), 1)
+                             units = "YEARS", round_down = TRUE), 1)
   expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "WEEKS", date_class = "period",
-                             round_down = TRUE), 55)
-})
-
-test_that("date_class has to be either period or duration", {
-  expect_error(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "years", date_class = "periods",
-                             round_down = TRUE))
-  expect_error(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "years", date_class = "durations",
-                             round_down = TRUE))
-})
-
-test_that("date_class works properly for duration", {
-  expect_equal(age_calculate(as.Date("2020-02-29"), as.Date("2022-04-08"),
-                             round_down = FALSE, date_class = "duration") * 365.25, 769)
+                             units = "MONTHS", round_down = TRUE), 12)
 })
 
 test_that("round_down works properly if it is FALSE", {
   expect_equal(age_calculate(as.Date("2021-02-01"), as.Date("2022-02-21"),
-                             units = "years", date_class = "period",
-                             round_down = FALSE), 1.05475702)
+                             units = "years", round_down = FALSE), 1.05475702)
 })
 
 test_that("Age is calculated correctly for a dataframe", {
