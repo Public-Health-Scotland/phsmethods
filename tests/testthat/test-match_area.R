fd1675068e83ad615a2e99a12be0f488bb7a67ef
Moohan <Moohan@users.noreply.github.com>
2022-09-09 14:26:54 +0000

    Style code

--- tests/testthat/test-match_area.R
+++ tests/testthat/test-match_area.R
@@ -1,36 +1,44 @@
 test_that("Returns the correct area names", {
-  expect_equal(match_area(c("S20000010",
-                            "S01002363",
-                            "S01004303",
-                            "S02000656",
-                            "S02001042",
-                            "S08000020",
-                            "S12000013",
-                            "S12000048",
-                            "S13002522",
-                            "S13002873",
-                            "S14000020",
-                            "S16000124",
-                            "S22000004")),
-               c("Eaglesham",
-                 "Marybank to Newvalley",
-                 "Elgin South Lesmurdie",
-                 "Govan and Linthouse",
-                 "Peebles North",
-                 "Grampian",
-                 "Na h-Eileanan Siar",
-                 "Perth and Kinross",
-                 "Dunoon",
-                 "Arbroath East and Lunan",
-                 "East Lothian",
-                 "Hamilton, Larkhall and Stonehouse",
-                 "Banff"))
+  expect_equal(
+    match_area(c(
+      "S20000010",
+      "S01002363",
+      "S01004303",
+      "S02000656",
+      "S02001042",
+      "S08000020",
+      "S12000013",
+      "S12000048",
+      "S13002522",
+      "S13002873",
+      "S14000020",
+      "S16000124",
+      "S22000004"
+    )),
+    c(
+      "Eaglesham",
+      "Marybank to Newvalley",
+      "Elgin South Lesmurdie",
+      "Govan and Linthouse",
+      "Peebles North",
+      "Grampian",
+      "Na h-Eileanan Siar",
+      "Perth and Kinross",
+      "Dunoon",
+      "Arbroath East and Lunan",
+      "East Lothian",
+      "Hamilton, Larkhall and Stonehouse",
+      "Banff"
+    )
+  )
 })
 
 test_that("Handles NA input values correctly", {
   expect_true(is.na(match_area(NA)))
-  expect_equal(match_area(c("S13002781", NA, NA, "S13003089")),
-               c("Ayr North", NA, NA, "Ayr North"))
+  expect_equal(
+    match_area(c("S13002781", NA, NA, "S13003089")),
+    c("Ayr North", NA, NA, "Ayr North")
+  )
 })
 
 test_that("Produces warnings for geography codes of invalid length", {
