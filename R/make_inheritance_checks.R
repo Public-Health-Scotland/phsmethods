5edc3fbecefa987a20546a131eaedc821a54b2dd
JoKPro <johannp@esplus.de>
2022-10-01 15:17:11 +0200

    cli messages, added option to ignore NULL params

--- R/make_inheritance_checks.R
+++ R/make_inheritance_checks.R
@@ -1,27 +1,36 @@
-#' Performs checks if elements have classes specified by 'target_classes' and prints out each element which is lacking the specified class or is NULL.
-#' Throws an exception if one element does not pass the checks.
-#' Element must have AT LEAST ONE of the specified classes to pass.
-#' @param elements a list object containing element_name=element pairs for each element. Element names must be provided or else they will be ignored.
+#' Performs checks if arguments have classes specified by 'target_classes' and prints out each argument which is lacking the specified class or is NULL.
+#' Throws an exception if one argument does not pass the checks.
+#' Argument must have AT LEAST ONE of the specified classes to pass.
+#' @param arguments a list object containing argument_name=argument pairs for each argument. Argument names must be provided or else they will be ignored.
 #' @param target_classes array of classes (string).
 
-make_inheritance_checks <- function(elements, target_classes) {
+make_inheritance_checks <- function(arguments, target_classes, ignore_null = T) {
   caller_func <- deparse(sys.calls()[[sys.nframe()-1]])
-  failures <- lapply(names(elements), FUN = function(element) {
-    if (is.null(elements[[element]])) {
-      return(paste0("Element ", "'", element, "'", " is null."))
+  failures <- lapply(names(arguments), FUN = function(argument) {
+    if (is.null(arguments[[argument]])) {
+      if (ignore_null) {
+        cli_alert_info("{.fn make_inheritance_checks}: Argument {.arg { argument }} is {.val NULL}, but argument {.strong {.code ignore_null = TRUE}}.")
+        return(NULL)
+        }
+      else { return(glue("Argument {.arg {.strong {% argument %}}} is {.val NULL} and argument {.strong {.code ignore_null = FALSE}}", .open = "{%", .close = "%}")) }
     }
-    if (!inherits(elements[[element]], target_classes)) {
-      return(paste0("Argument: ", "'", element, "'", " must have class: ", paste0(target_classes, collapse = " or ")))
+    if (!inherits(arguments[[argument]], target_classes) & !is.null(arguments[[argument]])) {
+      return(glue("Argument {.arg {.strong {% argument %}}} has class {.cls {% class(arguments[[argument]]) %}}, but must have any of classes {.cls {target_classes}}", .open = "{%", .close = "%}"))
     }
   })
-  names(failures) <- names(elements)
+  names(failures) <- names(arguments)
   failures <- Filter(function(x) !is.null(x), failures)
   if (!all(sapply(failures, is.null))) {
-    failures_out <- sapply(names(failures), FUN = function(element) {
-      if(!is.null(failures[[element]])) {
-        return(paste0(paste0(failures[[element]], collapse = "\n"), "\nClasses of ", "'", element, "': ", class(elements[[element]]), "\n"))
+    failures_out <- sapply(names(failures), FUN = function(argument) {
+      if(!is.null(failures[[argument]])) {
+        return(failures[[argument]])
       }
     })
-    stop(cat(paste0("[make_inheritance_checks] failed in function call: ", "'", caller_func, "'", ":\n", paste0(failures_out, collapse = "\n"))))
+    cli_rule()
+    cli_abort(c(
+      "x" = "inheritance checks {col_red('failed')} in function call: {.strong {.code {caller_func}}} due to following errors:",
+      failures_out
+    ))
   }
+  cli_alert_success("inheritance checks {col_green('passed')}")
 }
