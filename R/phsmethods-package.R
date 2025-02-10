251af9ed2221f28b7c70199ef99d17d4c1f32890
nicolc11 <nicolaos.christofidis@phs.scot>
2025-02-10 10:35:22 +0000

    Merge conflict potential fix.

---
+++ R/phsmethods-package.R
@@ -0,0 +1,12 @@
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+#' @importFrom rlang .data
+#' @importFrom rlang %||%
+#' @importFrom tibble tibble
+#' @importFrom lifecycle deprecated
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
