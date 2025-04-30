240369a3a22aa1f0702af0861cae6b0d11421164
James McMahon <james.mcmahon@phs.scot>
2025-04-30 12:12:11 +0000

    Add more tests

--- tests/testthat/test-age_calculate.R
+++ tests/testthat/test-age_calculate.R
@@ -1,3 +1,62 @@
+test_that("Missing arguments produce an error", {
+  expect_error(age_calculate(
+    end = as.Date("2022-02-21"),
+    units = "years",
+    round_down = TRUE
+  ), regexp = "argument \"start\" is missing")
+})
+
+test_that("End defaults to current date when not supplied", {
+  expect_equal(
+    age_calculate(
+      as.Date(Sys.Date() - 365),
+      units = "years",
+      round_down = TRUE
+    ),
+    1
+  )
+})
+
+test_that("Invalid units produce an error", {
+  expect_error(age_calculate(
+    as.Date("2021-02-01"),
+    as.Date("2022-02-21"),
+    units = "days",
+    round_down = TRUE
+  ), regexp = "should be one of \"years\", \"months\"")
+})
+
+test_that("NA values in start or end ", {
+  expect_equal(age_calculate(
+    as.Date(c(NA)),
+    as.Date("2022-02-21"),
+    units = "years",
+    round_down = TRUE
+  ), NA_real_)
+  expect_equal(age_calculate(
+    as.Date("2021-02-01"),
+    as.Date(c(NA)),
+    units = "years",
+    round_down = TRUE
+  ), NA_real_)
+})
+
+test_that("Vectorized inputs of unequal lengths", {
+  expect_equal(age_calculate(
+    as.Date(c("2021-02-01", "2020-02-29")),
+    as.Date("2022-02-21"),
+    units = "years",
+    round_down = TRUE
+  ), c(1, 1))
+
+  expect_error(age_calculate(
+    as.Date("2021-02-21"),
+    as.Date(c("2022-02-01", "2021-02-28")),
+    units = "years",
+    round_down = TRUE
+  ))
+})
+
 test_that("Age is calculated correctly for all units", {
   expect_equal(
     age_calculate(
@@ -100,6 +159,18 @@ test_that("round_down works properly if it is FALSE", {
   )
 })
 
+test_that("Leap year calculation with round_down = FALSE", {
+  expect_equal(
+    age_calculate(
+      as.Date("2020-02-29"),
+      as.Date("2021-03-01"),
+      units = "years",
+      round_down = FALSE
+    ),
+    1.002732240
+  )
+})
+
 test_that("Age is calculated correctly for a dataframe", {
   expect_equal(age_calculate(
     as.Date(
@@ -172,4 +243,180 @@ test_that("Return warning if age is greater than 130", {
     ),
     units = "months"
   ), regexp = "There are ages greater than 130 years\\.$")
+})
+
+test_that("Timezone differences are handled correctly", {
+  expect_equal(
+    age_calculate(
+      as.POSIXct("2021-02-01 12:00:00", tz = "UTC"),
+      as.POSIXct("2022-02-01 12:00:00", tz = "America/New_York"),
+      units = "years",
+      round_down = TRUE
+    ),
+    1
+  )
+})
+
+test_that("Mixed date classes are handled correctly", {
+  expect_equal(
+    age_calculate(
+      as.Date("2021-02-01"),
+      as.POSIXct("2022-02-21 12:00:00"),
+      units = "years",
+      round_down = TRUE
+    ),
+    1
+  )
+})
+
+test_that("Rounding behavior for borderline cases", {
+  expect_equal(
+    age_calculate(
+      as.Date("2021-02-01"),
+      as.Date("2022-02-21"),
+      units = "years",
+      round_down = FALSE
+    ),
+    1.0546448
+  )
+  expect_equal(
+    age_calculate(
+      as.Date("2021-02-01"),
+      as.Date("2022-02-01"),
+      units = "years",
+      round_down = TRUE
+    ),
+    1
+  )
+})
+
+test_that("Large date ranges are handled correctly", {
+  expect_equal(
+    age_calculate(
+      as.Date("1000-01-01"),
+      as.Date("2000-01-01"),
+      units = "years",
+      round_down = TRUE
+    ),
+    1000
+  ) |>
+    expect_warning("There are ages greater than 130 years\\.$")
+})
+
+
+test_that("Age is calculated correctly when start date is a leap day", {
+  expect_equal(
+    age_calculate(
+      as.Date("2020-02-29"),
+      as.Date("2021-02-28"),
+      units = "years",
+      round_down = TRUE
+    ),
+    0
+  )
+  expect_equal(
+    age_calculate(
+      as.Date("2020-02-29"),
+      as.Date("2021-03-01"),
+      units = "years",
+      round_down = TRUE
+    ),
+    1
+  )
+})
+
+test_that("Age is calculated correctly when end date is a leap day", {
+  expect_equal(
+    age_calculate(
+      as.Date("2019-02-28"),
+      as.Date("2020-02-29"),
+      units = "years",
+      round_down = TRUE
+    ),
+    1
+  )
+  expect_equal(
+    age_calculate(
+      as.Date("2018-03-01"),
+      as.Date("2020-02-29"),
+      units = "years",
+      round_down = TRUE
+    ),
+    1
+  )
+})
+
+test_that("Age is calculated correctly when both start and end dates are leap days", {
+  expect_equal(
+    age_calculate(
+      as.Date("2020-02-29"),
+      as.Date("2024-02-29"),
+      units = "years",
+      round_down = TRUE
+    ),
+    4
+  )
+})
+
+test_that("Age is calculated correctly around leap years when rounding down", {
+  expect_equal(
+    age_calculate(
+      as.Date("2020-02-29"),
+      as.Date("2024-02-28"),
+      units = "years",
+      round_down = TRUE
+    ),
+    3
+  )
+})
+
+test_that("Age is calculated correctly for leap day with months as units", {
+  expect_equal(
+    age_calculate(
+      as.Date("2020-02-29"),
+      as.Date("2021-02-28"),
+      units = "months",
+      round_down = TRUE
+    ),
+    11
+  )
+  expect_equal(
+    age_calculate(
+      as.Date("2020-02-29"),
+      as.Date("2021-03-01"),
+      units = "months",
+      round_down = TRUE
+    ),
+    12
+  )
+  expect_equal(
+    age_calculate(
+      as.Date("2020-02-29"),
+      as.Date("2024-02-29"),
+      units = "months",
+      round_down = TRUE
+    ),
+    48
+  )
+})
+
+test_that("Age is calculated correctly when leap day is in the middle of the interval", {
+  expect_equal(
+    age_calculate(
+      as.Date("2019-06-01"),
+      as.Date("2020-03-01"),
+      units = "years",
+      round_down = TRUE
+    ),
+    0
+  )
+  expect_equal(
+    age_calculate(
+      as.Date("2019-06-01"),
+      as.Date("2020-03-01"),
+      units = "months",
+      round_down = TRUE
+    ),
+    9
+  )
 })
